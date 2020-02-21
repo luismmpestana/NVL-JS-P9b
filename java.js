61bf35360eb01249b03d6c69b757d76069370c2d
Luis Miguel <luismolinapestana@gmail.com>
2020-02-21 15:21:06 +0100

    arreglos array

---
+++ java.js
@@ -0,0 +1,15 @@
+var Dias=
+	["Lunes", "Martes", "Miercoles", "Jueves", "Viernes","Sabado","Domingo"]
+
+var Ciudad= ["Madrid", "Sevilla", "Barcelona", "Valencia", "Zaragoza", "Bilbao", "Donosti"]
+
+var Gira= [
+		[Dias[0] + Ciudad[0]],
+		[Dias[1] + Ciudad[1]],
+		[Dias[2] + Ciudad[2]],
+		[Dias[3] + Ciudad[3]],
+		[Dias[4] + Ciudad[4]],
+		[Dias[5] + Ciudad[5]],
+		[Dias[6] + Ciudad[6]],
+		]
+document.write (Gira);
